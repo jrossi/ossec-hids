7d8557222fe1840d1149256fd5b245b2d9cf22af
Jeremy Rossi <jeremy@jeremyrossi.com>
2014-03-17 15:57:23 -0400

    test your scripts jeremy

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,15 +1,13 @@
 
-function iferrdie() {
-  if [[ $1 ne 0  ]]; then 
-    echo "ERROR: $2"
-    exit $1 
-  fi
+function die() {
+  echo "ERROR: $1"
+  exit 1
 }
 
-if [[ $OSSEC_TYPE == "All" ]]; then 
-  cp ./etc/travis-preloaded-vars.conf ./etc/preloaded-vars.conf || iferrdie($?, "create preloaded-vars.conf failed")
-  sudo ./install.sh || iferrdie($?, "sudo ./install.sh: Failed")
-  (cd contrib/ossec-testing && sudo python runtests.py ) || iferrdie($?, "runtests failed")
+if [[ $OSSEC_TYPE == "all" ]]; then 
+  cp ./etc/travis-preloaded-vars.conf ./etc/preloaded-vars.conf || die "create preloaded-vars.conf failed"
+  sudo ./install.sh || die "sudo ./install.sh: Failed"
+  (cd contrib/ossec-testing && sudo python runtests.py ) || die"runtests failed"
 else 
   exit 0
 fi
